71399a78d755fabaa1cad096cac1e9660b1d5abe
Daniel <herrnichols@gmail.com>
2019-08-12 15:56:16 -0400

    infinity and beyond drill

--- daniel-joey-function-drills-2.js
+++ daniel-joey-function-drills-2.js
@@ -5,4 +5,20 @@ function jediName(firstName, lastName){
 }
 jediName('joseph', 'romo');
 
-f
+function beyond(num) {
+  if (num === Infinity || num === -Infinity) {
+    console.log('And beyond');
+  }
+  if (num > 0 && num < Infinity) {
+    console.log('To infinity');
+  }
+  if (num < 0 && num > -Infinity) {
+    console.log('To negative infinity')
+  }
+  if (num === 0){
+    console.log('Staying home');
+  }
+}
+
+
+
