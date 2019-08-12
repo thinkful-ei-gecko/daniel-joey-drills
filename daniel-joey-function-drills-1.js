355b80d8664ca3d5535dae8d74d8229bdd965059
Daniel <herrnichols@gmail.com>
2019-08-12 14:36:23 -0400

    added YOB function

--- daniel-joey-function-drills-1.js
+++ daniel-joey-function-drills-1.js
@@ -1,5 +1,15 @@
-function createGreeting(){
-    console.log('Hi, my name is Joey and I'm 23 years old')
+/* eslint-disable strict */
+
+function getYearOfBirth(age) {
+  return 2019 - age;
 }
 
-createGreeting()
+function createGreeting(name, age){
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I\'m ${age} years old. 
+    I was born in ${yob}`;
+  
+}
+
+const greeting1 = createGreeting();
+console.log(greeting1);
