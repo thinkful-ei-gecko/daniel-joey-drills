7f456d8e1d089da6eae241c07b35e8e1f7b17507
Daniel <herrnichols@gmail.com>
2019-08-12 15:24:22 -0400

    delinted

--- daniel-joey-function-drills-1.js
+++ daniel-joey-function-drills-1.js
@@ -1,17 +1,16 @@
 /* eslint-disable strict */
 
 function getYearOfBirth(age) {
-  
   return 2019 - age;
 }
 
 function createGreeting(name, age){
-    if (typeof age !== Number){
-        throw new TypeError('Needs to be a number')
-    }
-    if(typeof name !== String){
-        throw new TypeError('needs to be letters')
-    }
+  if (typeof age !== Number){
+    throw new TypeError('Needs to be a number')
+  }
+  if(typeof name !== String){
+    throw new TypeError('needs to be letters')
+  }
   if (name === undefined || age === undefined) {
     throw new Error ('Arguments not Valid');
   }
@@ -19,17 +18,14 @@ function createGreeting(name, age){
     throw new Error('Age can not be negative');}
 
   const yob = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I\'m ${age} years old. 
+  return `Hi, my name is ${name} and I'm ${age} years old. 
     I was born in ${yob}`;
   
 }
 
-try{
+try {
   const greeting1 = createGreeting('joey',23);
-
-  console.log(greeting1)
-  catch(e){console.log('error')}
-}
-  
-
+  console.log(greeting1);}
+catch(error){
+  console.error(error);}
 
